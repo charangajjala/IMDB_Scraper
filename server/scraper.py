b9e19a62d79b6a3c9ed130ba5ff85a7199f4bdbe
charangajjala <charangajjala7@gmail.com>
2022-08-31 18:17:26 +0530

    setup chrome buildpacks

--- server/scraper.py
+++ server/scraper.py
@@ -21,7 +21,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--no-sandbox") 
 
 
 kwd = None
@@ -47,7 +47,7 @@ class Scraper:
         global driver, url
         try:
             DRIVER = webdriver.Chrome(
-                executable_path=ChromeDriverManager().install(),
+                executable_path=os.environ.get("CHROMEDRIVER_PATH"),
                 chrome_options=chrome_options,
             )
             DRIVER.maximize_window()
