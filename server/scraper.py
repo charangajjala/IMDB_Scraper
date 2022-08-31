de8056ec7fb7c283967526a7c5859f2c3aaa9407
charangajjala <charangajjala7@gmail.com>
2022-08-31 17:22:25 +0530

    setup chrome buildpacks

--- server/scraper.py
+++ server/scraper.py
@@ -13,13 +13,18 @@ from selenium.common.exceptions import (
 from .locators import Locator
 from .exception_handling import MyException
 from .mydb import MyDB
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
-chrome_executable_path = ChromeDriverManager().install()
+# chrome_executable_path = ChromeDriverManager().install()
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("disable-dev-shm-usage")
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
 kwd = None
 type = None
@@ -42,7 +47,7 @@ class Scraper:
         global driver, url
         try:
             DRIVER = webdriver.Chrome(
-                executable_path=chrome_executable_path,
+                executable_path=os.environ.get("CHROMEDRIVER_PATH"),
                 chrome_options=chrome_options,
             )
             DRIVER.maximize_window()
