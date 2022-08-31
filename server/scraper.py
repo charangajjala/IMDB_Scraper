4ce33d8badc4224e8845b29ca91005a286d54b59
charangajjala <charangajjala7@gmail.com>
2022-08-31 17:45:20 +0530

    setup chrome buildpacks

--- server/scraper.py
+++ server/scraper.py
@@ -18,10 +18,11 @@ import os
 
 # chrome_executable_path = ChromeDriverManager().install()
 chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+
 
 kwd = None
 type = None
